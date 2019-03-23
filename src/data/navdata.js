fb7a8681783518d7262adeebed093182b61eac4a
Stefano <stefan.emailacct@gmail.com>
2019-03-23 16:55:45 +0000

    [navigation]component and data file

---
+++ src/data/navdata.js
@@ -0,0 +1,34 @@
+export const navigation =
+  {
+    label0: {
+      name:'SLInc.®', linkUrl: '/about'
+    },
+
+    label1: {
+      name: 'Home', linkUrl: '/'
+    },
+
+    label2: {
+      name:'Page one', linkUrl: '/pageone'
+    },
+
+    label3: {
+      name: 'Page two', linkUrl: '/pagetwo'
+    },
+
+    label4: {
+      name: 'Dropdown', linkUrl: '/#'
+    },
+
+    label5: {
+      name: 'Action1', linkUrl: '/#Action1'
+    },
+
+    label6: {
+      name: 'Action2', linkUrl: '/#Action2'
+    },
+
+    label7: {
+      name: 'Action3', linkUrl: '/#Action3'
+    },
+  };
