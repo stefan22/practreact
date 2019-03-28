4084ddaa97effe51ec6a26fa9ba0b94b2ca2bca0
Stefano <stefan.emailacct@gmail.com>
2019-03-28 00:08:22 +0000

    [helpers]added

---
+++ src/components/helpers.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+export const DataBottomTable = (props) => {
+
+  return (
+
+    <div className='form-info'>
+      <table className='table table-sm table-dark'>
+        <thead>
+          <tr><th scope='row'>State form data</th></tr>
+          <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>First</th>
+            <th scope='col'>Last</th>
+            <th scope='col'>Job title</th>
+            <th scope='col'>Company</th>
+            <th scope='col'>Salary</th>
+            <th scope='col'>Preference</th>
+            <th scope='col'>Terms</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope='row'>1</th>
+            <td>{props.firstname ? props.firstname : 'n/a'}</td>
+            <td>{props.lastname ? props.lastname : 'n/a'}</td>
+            <td>{props.jobtitle ? props.jobtitle : 'n/a'}</td>
+            <td>{props.company ? props.company : 'n/a'}</td>
+            <td>{props.salary ? props.salary : '0'}</td>
+            <td>{props.preference ? props.preference : 'n/a'}</td>
+            <td>{props.terms ? 'true':'false'}</td>
+          </tr>
+          </tbody>
+      </table>
+    </div>);
+
+};
+
+
+export let options = [
+
+  {
+    name: 'Full-time',
+    value: 'full-time',
+  },
+  {
+    name: 'Part-time',
+    value: 'part-time',
+  },
+  {
+    name: 'Remote',
+    value: 'remote',
+  }
+
+];
+
+
+export const IsFormDataValid = (props) => {
+  if (
+    props.jobtitle.length >= 7 && //jobtitle at least 7 length
+    props.company.length >= 3 &&  //company name at least 3 length
+    props.salary > 0 &&           //salary anything greater than zero
+    props.preference !== '' &&    //role preference pick one
+    props.terms !== false         //must accept terms
+
+  ) {
+    return true;
+  }
+
+}
+
--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -3,27 +3,11 @@ import '../../scss/form.scss';
 import TextInputField from '../form/TextInputField';
 import SelectOption from '../form/SelectOption';
 import CheckboxInputField from '../form/CheckboxInputField';
-
-let options=[
-  {
-    name: 'Full-time',
-    value: 'full-time',
-  },
-  {
-    name: 'Part-time',
-    value: 'part-time',
-  },
-  {
-    name: 'Remote',
-    value: 'remote',
-  }
-];
-
+import {DataBottomTable, options, IsFormDataValid} from '../helpers';
 
 class ContactForm extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       isFormVisible: true,
       firstname: '',
@@ -33,7 +17,6 @@ class ContactForm extends Component {
       salary: 0,
       preference: '',
       terms: false,
-
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleFormSubmission = this.handleFormSubmission.bind(this);
@@ -68,28 +51,11 @@ class ContactForm extends Component {
   }
 
 
-  isFormDataValid = (state) => {
-    if (
-      state.jobtitle.length >= 7 && //jobtitle at least 7 length
-      state.company.length >= 3 &&  //company name at least 3 length
-      state.salary > 0 &&           //salary anything greater than zero
-      state.preference !== '' &&    //role preference pick one
-      state.terms !== false         //must accept terms
-
-    ) {
-      return true;
-    }
-
-  }
-
-
-
 
   render() {
     const {
       isFormVisible,firstname,lastname,jobtitle,company,salary,preference,terms} = this.state;
-    console.log(this);
-    const enableButton = this.isFormDataValid(this.state);
+    const enableButton = IsFormDataValid(this.state);
 
     return (
       <div className='container'>
@@ -113,7 +79,6 @@ class ContactForm extends Component {
 
               {/* keeps values in form */}
               <div style={{visibility: isFormVisible ? 'visible': 'hidden'}}>
-
                 <form onSubmit={this.handleFormSubmission}>
 
                   <TextInputField
@@ -167,35 +132,17 @@ class ContactForm extends Component {
               </div>
             </div>
 
-            <div className='form-info'>
-                <table className='table table-sm table-dark'>
-                  <thead>
-                    <tr><th scope='row'>State form data</th></tr>
-                    <tr>
-                      <th scope='col'>#</th>
-                      <th scope='col'>First</th>
-                      <th scope='col'>Last</th>
-                      <th scope='col'>Job title</th>
-                      <th scope='col'>Company</th>
-                      <th scope='col'>Salary</th>
-                      <th scope='col'>Preference</th>
-                      <th scope='col'>Terms</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope='row'>1</th>
-                      <td>{firstname ? firstname : 'n/a'}</td>
-                      <td>{lastname ? lastname : 'n/a'}</td>
-                      <td>{jobtitle ? jobtitle : 'n/a'}</td>
-                      <td>{company ? company : 'n/a'}</td>
-                      <td>{salary ? salary : '0'}</td>
-                      <td>{preference ? preference : 'n/a'}</td>
-                      <td>{terms ? 'true':'false'}</td>
-                    </tr>
-                   </tbody>
-                </table>
-              </div>
+
+            <DataBottomTable
+              firstname={firstname}
+              lastname={lastname}
+              jobtitle={jobtitle}
+              company={company}
+              salary={salary}
+              preference={preference}
+              terms={terms}
+
+            />
 
           </div>
         </div>
@@ -207,3 +154,5 @@ class ContactForm extends Component {
 
 
 export default ContactForm;
+
+
